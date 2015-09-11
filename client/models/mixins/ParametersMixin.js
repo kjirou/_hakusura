64cf2da2957c0421ad07ce9246a2406616c51023
kjirou <kjirou.web@gmail.com>
2015-09-11 15:27:17 +0900

    Apply parameters to VehicleModel

--- client/models/mixins/ParametersMixin.js
+++ client/models/mixins/ParametersMixin.js
@@ -8,12 +8,12 @@ const ParametersMixin = {};
 
 defineIntegerParameter(ParametersMixin, 'weight');
 defineIntegerParameter(ParametersMixin, 'maxWeight');
-defineIntegerParameter(ParametersMixin, 'maxFuel');
-defineRateParameter(ParametersMixin, 'fuelEfficiencyRate');
-defineIntegerParameter(ParametersMixin, 'maxAmmo');
 defineIntegerParameter(ParametersMixin, 'maxArmorPower');
-defineIntegerParameter(ParametersMixin, 'shieldHardness');
 defineIntegerParameter(ParametersMixin, 'maxShieldEnergy');
+defineIntegerParameter(ParametersMixin, 'maxFuel');
+defineIntegerParameter(ParametersMixin, 'maxAmmo');
+defineIntegerParameter(ParametersMixin, 'shieldHardness');
+defineRateParameter(ParametersMixin, 'fuelEfficiencyRate');
 defineIntegerParameter(ParametersMixin, 'operability');
 defineIntegerParameter(ParametersMixin, 'speed');
 defineIntegerParameter(ParametersMixin, 'maxMainGunCount');
--- client/models/vehicles/VehicleModel.js
+++ client/models/vehicles/VehicleModel.js
@@ -1,58 +1,131 @@
+import { aggregators } from 'rpgparameter';
+
 import Model from '../Model';
 
 
 export default class VehicleModel extends Model {
 
-  constructor() {
-    this._vehicleFrame = undefined;
-    this._engine = undefined;
-    this._fuelTank = undefined;
-    this._wheels = undefined;
-    this._armor = undefined;
+  constructor(...args) {
+    super(...args);
+
+    this._vehicleFrame = null;
+    this._armor = null;
+    this._engine = null;
+    this._fuelTank = null;
+    this._wheels = null;
     this._mainGuns = [];
     this._subGuns = [];
     this._staffs = [];
   }
 
+  _getEquipmentList() {
+    return [
+      this._vehicleFrame,
+      this._armor,
+      this._engine,
+      this._fuelTank,
+      this._wheels,
+      ...(this._mainGuns),
+      ...(this._subGuns),
+    ].filter(v => Boolean(v));
+  }
+
   get weight() {
+    let params = this._getEquipmentList().map(v => v.getWeight());
+    return aggregators.aggregateNumbers(params);
   }
 
   get maxWeight() {
-    return 2000;
-  }
-
-  get fuel() {
-  }
-
-  get maxFuel() {
-  }
-
-  get armorPower() {
-  }
-
-  get shieldPower() {
+    let params = this._getEquipmentList().map(v => v.getMaxWeight());
+    return aggregators.aggregateNumbers(params);
   }
 
   get maxArmorPower() {
-    return 1;
+    let params = [
+      this._vehicleFrame
+    ].map(v => v.getMaxArmorPower());
+    return aggregators.aggregateNumbers(params);
+  }
+
+  get maxShieldEnergy() {
+    let params = this._getEquipmentList().map(v => v.getMaxShieldEnergy());
+    return aggregators.aggregateNumbers(params);
+  }
+
+  get maxFuel() {
+    let params = this._getEquipmentList().map(v => v.getMaxFuel());
+    return aggregators.aggregateNumbers(params);
+  }
+
+  get maxAmmo() {
+    let params = this._getEquipmentList().map(v => v.getMaxAmmo());
+    return aggregators.aggregateNumbers(params);
+  }
+
+  get fuelEfficiencyRate() {
+    let params = this._getEquipmentList().map(v => v.getFuelEfficiencyRate());
+    return aggregators.aggregateRates(params);
+  }
+
+  get shieldHardness() {
+    let params = this._getEquipmentList().map(v => v.getShieldHardness());
+    return aggregators.aggregateNumbers(params);
   }
 
   get operability() {
-    return 1;
+    let params = this._getEquipmentList().map(v => v.getOperability());
+    return aggregators.aggregateNumbers(params);
   }
 
-  get maxAmmoCount() {
+  get speed() {
+    let params = this._getEquipmentList().map(v => v.getSpeed());
+    return aggregators.aggregateNumbers(params);
   }
 
   get maxMainGunCount() {
-    return 1;
+    let params = this._getEquipmentList().map(v => v.getMaxMainGunCount());
+    return aggregators.aggregateNumbers(params);
   }
 
   get maxSubGunCount() {
-    return 1;
+    let params = this._getEquipmentList().map(v => v.getMaxSubGunCount());
+    return aggregators.aggregateNumbers(params);
   }
 
   get maxStaffCount() {
-    return 5;
+    let params = this._getEquipmentList().map(v => v.getMaxStaffCount());
+    return aggregators.aggregateNumbers(params);
+  }
+
+  /*
+   * Cut variable state by limits of min/max/etc
+   */
+  _adaptState() {
+    // TODO
+  }
+
+  set vehicleFrame(v) {
+    this._vehicleFrame = v;
+    this._adaptState();
+  }
+
+  set armor(v) {
+    this._armor = v;
+    this._adaptState();
+  }
+
+  set engine(v) {
+    this._engine = v;
+    this._adaptState();
+  }
+
+  set fuelTank(v) {
+    this._fuelTank = v;
+    this._adaptState();
+  }
+
+  set wheels(v) {
+    this._wheels = v;
+    this._adaptState();
   }
 }
