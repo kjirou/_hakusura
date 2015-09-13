40090396a6d7406b74bb82aff04446727b66edf8
kjirou <kjirou.web@gmail.com>
2015-09-13 18:23:08 +0900

    Show stage selection mocks

--- client/components/pages/WelcomePageComponent.js
+++ client/components/pages/WelcomePageComponent.js
@@ -1,20 +1,31 @@
+import _s from 'underscore.string';
+
 import shared from '../shared';
+import { STAGE_SELECTION } from 'consts/keys';
 
 
 export default class WelcomePageComponent extends React.Component {
 
   render() {
 
-    let content = '';
+    let lines = [];
 
     // Title
-    content += '{magenta-fg}Fury Road{/}';
+    lines.push('{magenta-fg}Fury Road{/} - A simple Roguelike game');
+    lines.push('');
+
+    // Choices
+    Object.keys(STAGE_SELECTION).sort().forEach((keySequence) => {
+      let line = `[{magenta-fg}${keySequence}{/}] `;
+      line += _s.titleize(_s.humanize(STAGE_SELECTION[keySequence]));
+      lines.push(line);
+    });
 
     let props = Object.assign({}, shared.pageProps);
 
     return (
       <box {...props} >
-        {content}
+        {lines.join('\n')}
       </box>
     );
   }
---
+++ client/consts/keys.js
@@ -0,0 +1,16 @@
+const KEYS = {
+
+  STAGE_SELECTION: {
+    '1': 'a_small_sized_car',
+    '2': 'a_medium_sized_car',
+    '3': 'a_large_sized_car',
+    'a': 'interceptor',
+    'b': 'the_war_rig',
+    'c': 'the_nux_car',
+    'd': 'the_peacemaker',
+    'e': 'doof_wagon',
+  }
+};
+
+
+export default KEYS;
