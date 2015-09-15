c76a47315342a5d7704cf7d5e662f8c339bd7a1c
kjirou <kjirou.web@gmail.com>
2015-09-16 02:22:45 +0900

    Show vehicle image

--- client/components/pages/GamePageComponent.js
+++ client/components/pages/GamePageComponent.js
@@ -1,16 +1,87 @@
+import _s from 'underscore.string';
+
 import shared from '../shared';
+import VehicleComponent from '../VehicleComponent';
+
+
+class StatusBar extends React.Component {
+
+  render() {
+
+    let underlineProps = {
+      key: 'underline',
+      top: 1,
+      left: 0,
+      width: '100%',
+      height: 1,
+      tags: true,
+      style: {
+        fg: 'white',
+        bg: 'black',
+      },
+      content: _s.repeat('\u2500', 100),
+    };
+
+    let props = {
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: 2,
+      tags: true,
+      style: {
+        fg: 'white',
+        bg: 'black',
+      },
+    };
+
+    let content = '';
+    // Mileage
+    content += () => {
+      return ` ${_s.pad(this.props.mileage, 4)}/${_s.pad(this.props.maxMileage, 4)} km \u2502 `;
+    }();
+    // Others
+    content += [
+      `Fuel:${_s.pad(this.props.fuel, 2)}`,
+      `Ammo:${_s.pad(this.props.ammo, 2)}`,
+    ].join(', ');
+    props.content = content;
+
+    return (
+      <box {...props}>
+        <box {...underlineProps} />
+      </box>
+    );
+  }
+}
 
 
 export default class GamePageComponent extends React.Component {
 
   render() {
 
-    const props = Object.assign({}, shared.pageProps, {
-      content: this.constructor.name,
-    });
+    const props = Object.assign({
+      // tmp
+      mileage: 199,
+      maxMileage: 9999,
+      fuel: 18,
+      ammo: 72,
+    }, shared.pageProps);
+
+    const statusBarProps = Object.assign({
+      key: 'status_bar',
+    }, props);
+
+    const playerVehicleProps = {
+      key: 'player_vehicle',
+      top: 4,
+      left: 2,
+    };
 
     return (
-      <box {...props} />
+      <box {...props}>
+        <StatusBar {...statusBarProps} />
+        <VehicleComponent {...playerVehicleProps} />
+      </box>
     );
   }
 }
