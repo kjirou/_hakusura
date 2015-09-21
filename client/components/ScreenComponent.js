373e1799acf30562ac03e11ed6d24c2ab11dcf80
kjirou <kjirou.web@gmail.com>
2015-09-21 14:06:02 +0900

    Remove waste codes

--- client/components/ScreenComponent.js
+++ client/components/ScreenComponent.js
@@ -5,68 +5,6 @@ import _s from 'underscore.string';
 const SCREEN_WIDTH = 80;
 const SCREEN_HEIGHT = 32;
 
-class ConsoleComponent extends React.Component {
-
-  render() {
-
-    let props = {
-      top: SCREEN_HEIGHT - 2,
-      left: 0,
-      width: '100%',
-      height: SCREEN_HEIGHT,
-      style: {
-        fg: 'white',
-        transparent: true,
-      },
-    };
-
-    let lineProps = {
-      key: 'line',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: 1,
-      style: {
-        fg: 'white',
-      },
-      content: _s.repeat('\u2500', 100),
-    };
-
-    let shellProps = {
-      key: 'inputter',
-      top: 1,
-      left: 0,
-      width: '100%',
-      height: SCREEN_HEIGHT - 1,
-      tags: true,
-      style: {
-        fg: 'white',
-        transparent: true,
-      },
-      content: '> Push [{green-fg}enter{/}] key to startkkkka',
-    };
-
-    //let content = '';
-    //// Mileage
-    //content += () => {
-    //  return ` ${_s.pad(this.props.mileage, 4)}/${_s.pad(this.props.maxMileage, 4)} km \u2502 `;
-    //}();
-    //// Others
-    //content += [
-    //  `Fuel:${_s.pad(this.props.fuel, 2)}`,
-    //  `Ammo:${_s.pad(this.props.ammo, 2)}`,
-    //].join(', ');
-    //props.content = content;
-
-    return (
-      <box { ...props } >
-        <box { ...lineProps } />
-        <box { ...shellProps } />
-      </box>
-    );
-  }
-}
-
 export default class ScreenComponent extends React.Component {
 
   static mergeLines(shellLines, outputLines) {
