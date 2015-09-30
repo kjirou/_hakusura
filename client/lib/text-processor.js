35b4b75cfdb54f1d685c1bdce72531ff1f6ee2b0
kjirou <kjirou.web@gmail.com>
2015-09-30 14:54:33 +0900

    Apply automatic carrier returns

--- client/lib/text-processor.js
+++ client/lib/text-processor.js
@@ -1,3 +1,7 @@
+import chalk from 'chalk';
+import _ from 'lodash';
+import _s from 'underscore.string';
+
 import ShellInputModes from 'consts/ShellInputModes';
 
 
@@ -8,4 +12,87 @@ export function generatePrompt(shellInputMode) {
     [ShellInputModes.INDEX]: 'index> ',
     [ShellInputModes.WIZARD]: 'wizard> ',
   }[shellInputMode] || '> ';
+}
+
+/*
+ * Insert \n automatically to blessed-content
+ *
+ * @return {string}
+ */
+export function applyAutomaticCarrierReturns(blessedContent, columnLength) {
+
+  // TODO: also consider ansi-colors
+  //       Ref) https://github.com/chalk/wrap-ansi
+  blessedContent = chalk.stripColor(blessedContent);
+
+  let replaced = '';
+  let currentColumnLength = 0;
+  let insideEscape = false;
+  let indexForInsert = null;
+  blessedContent.split('').forEach((c) => {
+
+    replaced += c;
+
+    if (c === '{') {
+      insideEscape = true;
+    }
+
+    // If next character exists ..
+    if (!insideEscape) {
+      // .. then, insert "\n" to kept index
+      if (indexForInsert !== null) {
+        replaced = _s.insert(replaced, indexForInsert, '\n');
+        currentColumnLength = 0;
+        indexForInsert = null;
+      }
+      currentColumnLength += 1;
+      // keep index
+      if (currentColumnLength === columnLength) {
+        indexForInsert = replaced.length;
+      }
+    }
+
+    if (c === '}') {
+      insideEscape = false;
+    }
+  });
+
+  return replaced;
+}
+
+export function linesToBlessedContent(lines, columnLength, rowLength) {
+  lines = lines.slice();
+
+  // pad lines for a case of not enough
+  _.range(rowLength).forEach(() => lines.push(''));
+
+  return lines
+    // cut the apparently extra lines for speed
+    .slice(0, rowLength)
+    // ensure that the beginning of lines is displayed to the top
+    .reverse()
+    .map(line => {
+
+      // normalize CRLF and CR to LF
+      line = line.replace(/(\r\n|\r)/g, '\n');
+
+      // 'long sentence' -> 'long sent\nence'
+      line = line
+        .split('\n')
+        .map(linePart => {
+          return applyAutomaticCarrierReturns(linePart, columnLength);
+        })
+        .join('\n')
+      ;
+
+      return line;
+    })
+    // expand multilined-line
+    // e.g. ['a\nb', 'c'] -> ['a', 'b', 'c']
+    .join('\n').split('\n')
+    // adjust lines into display
+    .slice(-rowLength)
+    // to string
+    .join('\n')
+  ;
 }
--- test/client/components/ScreenComponent.js
+++ test/client/components/ScreenComponent.js
@@ -1,31 +1,8 @@
 import assert from 'power-assert';
-import _s from 'underscore.string';
 
-import {
-  linesToContent,
-} from 'components/ScreenComponent';
-import { SCREEN_HEIGHT } from 'consts/ViewProps';
+import ScreenComponent from 'components/ScreenComponent';
 import { heading } from 'test/support/helpers';
 
 
 describe(heading(__filename), function() {
-
-  context('linesToContent', function() {
-
-    it('should be', function() {
-      assert.strictEqual(linesToContent([]), _s.repeat('\n', SCREEN_HEIGHT - 1));
-    });
-
-    it('should be reversed lines', function() {
-      assert.strictEqual(linesToContent(['first']), _s.repeat('\n', SCREEN_HEIGHT - 1) + 'first');
-    });
-
-    it('should be expanded multilined-lines', function() {
-      assert.strictEqual(linesToContent(['a\nb', 'c']), _s.repeat('\n', SCREEN_HEIGHT - 3) + 'c\na\nb');
-    });
-
-    it('should be normalized CRLF and CR', function() {
-      assert.strictEqual(linesToContent(['a\r\nb\rc']), _s.repeat('\n', SCREEN_HEIGHT - 3) + 'a\nb\nc');
-    });
-  });
 });
