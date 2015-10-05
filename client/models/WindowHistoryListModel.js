fd6713e99c8c1435647b73c16e2dfc7025e3ff3e
kjirou <kjirou.web@gmail.com>
2015-10-06 00:33:04 +0900

    Add stack method

--- client/models/WindowHistoryListModel.js
+++ client/models/WindowHistoryListModel.js
@@ -1,13 +1,41 @@
 import _ from 'lodash';
 
 import Model from './Model';
-import ListingMixinCreator from './mixins/ListingMixinCreator';
+import ListingMixin from './mixins/ListingMixin';
+import {
+  COMMAND_TREE,
+  MINIMIST_OPTIONS,
+} from 'client/consts/CommandDefinition';
+import { parse } from 'lib/command-parser';
 
+
+/*
+ * Is window conituning from pre-command?
+ *
+ * TODO: Can specify options for comparison
+ */
+export const _isWinodowContinuing = (command, preCommand) => {
+  const a = parse(COMMAND_TREE, MINIMIST_OPTIONS, command);
+  const b = parse(COMMAND_TREE, MINIMIST_OPTIONS, preCommand);
+  return a.commandId === b.commandId;
+};
 
 export default class WindowHistoryListModel extends Model {
 
-  add(command) {
+  constructor(...args) {
+    super(...args);
+    this._initializeListingMixin();
+  }
+
+  _add(command) {
     this._listObjects.push(command);
+  }
+
+  stack(command) {
+    const preCommand = this._listObjects[this._listObjects.length - 1] || null;
+    if (preCommand === null || !_isWinodowContinuing(command, preCommand)) {
+      this._add(command);
+    }
   }
 
   pop() {
@@ -15,4 +43,4 @@ export default class WindowHistoryListModel extends Model {
   }
 }
 
-Object.assign(WindowHistoryListModel.prototype, ListingMixinCreator());
+Object.assign(WindowHistoryListModel.prototype, ListingMixin);
--- test/client/models/WindowHistoryListModel.js
+++ test/client/models/WindowHistoryListModel.js
@@ -1,6 +1,8 @@
 import assert from 'power-assert';
 
-import WindowHistoryListModel from 'client/models/WindowHistoryListModel';
+import WindowHistoryListModel, {
+  _isWinodowContinuing,
+} from 'client/models/WindowHistoryListModel';
 
 import { heading } from 'test/support/helpers';
 
@@ -11,16 +13,45 @@ describe(heading(__filename), () => {
     let model = new WindowHistoryListModel();
   });
 
-  it('add, pop', () => {
+  it('_add, pop', () => {
     let model = new WindowHistoryListModel();
     assert.deepEqual(model.getListObjects(), []);
 
-    model.add('foo');
+    model._add('foo');
     assert.deepEqual(model.getListObjects(), ['foo']);
-    model.add('bar');
+    model._add('bar');
     assert.deepEqual(model.getListObjects(), ['foo', 'bar']);
 
     assert.strictEqual(model.pop(), 'bar');
     assert.deepEqual(model.getListObjects(), ['foo']);
+  });
+
+  it('_isWinodowContinuing', () => {
+    assert(_isWinodowContinuing('character index', 'character index'));
+    assert(_isWinodowContinuing('character index', 'character'));
+    assert(_isWinodowContinuing(' character index', 'character   index -a -b -c'));
+    assert(!_isWinodowContinuing('character index', 'character list'));
+    assert(!_isWinodowContinuing('character index', 'help'));
+  });
+
+  it('stack', () => {
+    let model = new WindowHistoryListModel();
+
+    model.stack('character index');
+    assert.deepEqual(model.getListObjects(), ['character index']);
+    model.stack('character index');
+    assert.deepEqual(model.getListObjects(), ['character index']);
+    model.stack('character list');
+    assert.deepEqual(model.getListObjects(), [
+      'character index',
+      'character list',
+    ]);
+    model.stack('character list');
+    model.stack('character index');
+    assert.deepEqual(model.getListObjects(), [
+      'character index',
+      'character list',
+      'character index',
+    ]);
   });
 });
